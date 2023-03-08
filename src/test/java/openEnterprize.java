b8e60a74ba4b4a04e831157448fd8b78bc03ad62
klyul <yuliya.v3333@gmail.com>
2023-03-08 14:32:35 +0300

    HW6

---
+++ src/test/java/openEnterprize.java
@@ -0,0 +1,16 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class openEnterprize {
+    @Test
+    void openEnterprize(){
+        open("https://github.com");
+        $("div.header-menu-wrapper").$(byText("Solutions")).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+        $(byText("Build like the best")).shouldBe(Condition.visible);
+    }
+}
